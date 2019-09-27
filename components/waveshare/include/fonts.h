e42187ae1552a03afb27f8e5e31f45ae435d22a9
Paolo Gatti <paolino1986@gmail.com>
2019-09-27 20:47:00 +0200

    added font40

--- components/waveshare/include/fonts.h
+++ components/waveshare/include/fonts.h
@@ -40,8 +40,8 @@
 #define __FONTS_H
 
 /* Max size of bitmap will based on a font24 (17x24) */
-#define MAX_HEIGHT_FONT         24
-#define MAX_WIDTH_FONT          17
+#define MAX_HEIGHT_FONT         40
+#define MAX_WIDTH_FONT          35
 #define OFFSET_BITMAP           54
 
 /* Includes ------------------------------------------------------------------*/
@@ -58,6 +58,7 @@ extern sFONT Font20;
 extern sFONT Font16;
 extern sFONT Font12;
 extern sFONT Font8;
+extern sFONT Font40;
 
 #endif /* __FONTS_H */
  
